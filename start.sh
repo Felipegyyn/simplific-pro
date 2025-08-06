6aa90e9d54ceda4fa79491d9b52bf6d748656b89
Felipe Gyyn <felipegyyn@gmail.com>
2025-08-06 18:21:41 -0300

    refactor: Separate build and start logic for deployment

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+echo "Running database migrations..."
+flask db upgrade
+
+echo "Creating default categories..."
+flask create-categories
+
+echo "Creating admin user..."
+flask create-admin
+
+echo "Starting Gunicorn server..."
+gunicorn src.main:app
