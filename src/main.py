946118c5cdc75de385b792eacb26a9a4d476704c
Felipe Gyyn <felipegyyn@gmail.com>
2025-08-09 01:27:16 -0300

    fix(backend): Correct mail server SSL/TLS configuration

--- src/main.py
+++ src/main.py
@@ -172,8 +172,8 @@ with app.app_context():
 # Configuração do Flask-Mail
     app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
     app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT', 587))
-    app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS', 'True').lower() in ['true', '1', 't']
-    app.config['MAIL_USE_SSL'] = os.getenv('MAIL_USE_SSL', 'False').lower() in ['true', '1', 't']
+    app.config['MAIL_USE_TLS'] = False
+    app.config['MAIL_USE_SSL'] = True
     app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
     app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
     app.config['MAIL_DEFAULT_SENDER'] = ('Simplific Pro', os.getenv('MAIL_USERNAME')) # Nome que aparecerá para o cliente
