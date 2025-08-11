8bd2fca8c2b13fd29915a14212cea94bc3e9e26e
Felipe Gyyn <felipegyyn@gmail.com>
2025-08-11 17:30:35 -0300

    fix(Frontend): Vencimento CreditCards alter

--- frontend/src/pages/CreditCards.jsx
+++ frontend/src/pages/CreditCards.jsx
@@ -527,7 +527,7 @@ const carregarFaturas = async (listaDeCartoes) => {
                             <SelectValue placeholder="Selecione o dia..." />
                           </SelectTrigger>
                           <SelectContent>
-                            {Array.from({ length: 31 }, (_, i) => i + 1).map(day => (
+                            {Array.from({ length: 30 }, (_, i) => i + 1).map(day => (
                               <SelectItem key={day} value={day.toString()}>
                                 Dia {day}
                               </SelectItem>
