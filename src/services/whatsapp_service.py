cc0cc73c01834a0e4e9795ebf994dcad16388742
Felipe Gyyn <felipegyyn@gmail.com>
2025-08-16 17:59:33 -0300

    Alter twilio template

--- src/services/whatsapp_service.py
+++ src/services/whatsapp_service.py
@@ -18,7 +18,7 @@ template_sids = {
     'planejamento_extrapolado': 'HX678b082253a03d42ceb842b2b703b5c3',
     'resumo_faturas': 'HX1e16cbb43cd749b1a3ec72fa3d461972',
     'resumo_lancamentos': 'HXed924fd8d2f7be3bc9bb1caaf70ffe47',
-    'welcome_simplific': 'HXe62289a548e76b5e2de3dd2daf238dc8'
+    'welcome_simplific': 'HX5be9120897732c8cbf75e0cafb5c4fca'
 }
 
 
