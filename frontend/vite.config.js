8da72c0e6a5f05a149997909879fd5578a7e4479
Felipe Gyyn <felipegyyn@gmail.com>
2025-09-07 10:19:37 -0300

    favicon

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  publicDir: 'public', // <-- ADICIONE ESTA LINHA AQUI
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
