8fb3aefc471d349e20d5f3acb5c8cef67a7bfa90
Felipegyyn <felipegyyn@gmail.com>
2025-09-30 13:30:21 -0300

    correção de sintase

--- src/main.py
+++ src/main.py
@@ -4,12 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 import cloudinary
 import cloudinary.uploader
-
 cloudinary.config(
-  cloud_name = os.getenv("CLOUDINARY_CLOUD_NAME"),
-  api_key = os.getenv("CLOUDINARY_API_KEY"),
-  api_secret = os.getenv("CLOUDINARY_API_SECRET"),
-  secure = True
+    cloud_name = os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key = os.getenv("CLOUDINARY_API_KEY"),
+    api_secret = os.getenv("CLOUDINARY_API_SECRET"),
+    secure = True
 # DON'T CHANGE THIS !!!
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
