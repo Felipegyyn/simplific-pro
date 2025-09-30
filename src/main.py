bfc15a685a9264ea82cbbc0374c57d3ae77d22cc
Felipegyyn <felipegyyn@gmail.com>
2025-09-30 13:35:07 -0300

    Correção

--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ cloudinary.config(
     api_key = os.getenv("CLOUDINARY_API_KEY"),
     api_secret = os.getenv("CLOUDINARY_API_SECRET"),
     secure = True
+)
 # DON'T CHANGE THIS !!!
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
