9bce2fe462687c3efe9608a3f67b118ccee48f63
Felipegyyn <felipegyyn@gmail.com>
2025-09-30 13:25:26 -0300

    Alter código main py

--- src/main.py
+++ src/main.py
@@ -2,6 +2,14 @@ import os
 import sys
 from dotenv import load_dotenv
 load_dotenv()
+import cloudinary
+import cloudinary.uploader
+
+cloudinary.config(
+  cloud_name = os.getenv("CLOUDINARY_CLOUD_NAME"),
+  api_key = os.getenv("CLOUDINARY_API_KEY"),
+  api_secret = os.getenv("CLOUDINARY_API_SECRET"),
+  secure = True
 # DON'T CHANGE THIS !!!
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
