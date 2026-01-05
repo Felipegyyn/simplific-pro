c26e9073fe5cb9696e6ce6a86932f9b7c4dc5bac
Felipe Viana <140557845+Felipegyyn@users.noreply.github.com>
2026-01-05 14:29:57 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ cloudinary.config(
     api_secret = os.getenv("CLOUDINARY_API_SECRET"),
     secure = True
 )
-# DON'T CHANGE THIS !!!
+# DON'T CHANGE THIS !!!!
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 
