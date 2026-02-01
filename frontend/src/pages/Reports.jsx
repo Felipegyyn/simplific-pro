8077f6dc81fc433227444eafbcb06543a856da4d
Felipegyyn <felipegyyn@gmail.com>
2026-02-01 12:58:29 -0300

    Ajuste v2

--- frontend/src/pages/Reports.jsx
+++ frontend/src/pages/Reports.jsx
@@ -163,7 +163,7 @@ useEffect(() => {
   }
 
   return (
-    <div className="sm:p-6 bg-gray-50 dark:bg-slate-900">
+    <div className="min-h-screen bg-gray-50/50 flex flex-col font-sans">
       
       {/* cabeçalho novo */}
       <PageHeader user={user} onLogout={onLogout} />
